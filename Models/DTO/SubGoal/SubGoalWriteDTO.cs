220ae15129f06ce3d39fee838a7d233c96fb51db
s325909 <jaso_cyril@hotmail.com>
2022-03-11 14:25:51 +0100

    FEAT: SubGoalDTOs with profile mapping

---
+++ Models/DTO/SubGoal/SubGoalWriteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeFit_BE.Models.DTO.SubGoal
+{
+    public class SubGoalWriteDTO
+    {
+        public bool Achieved { get; set; }
+
+        public int Workout { get; set; }
+    }
+}
