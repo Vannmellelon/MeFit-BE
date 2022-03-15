eef266eaf0dd20ee29334321359570d8200e8e90
s325909 <jaso_cyril@hotmail.com>
2022-03-15 13:30:26 +0100

    FEAT: Added LoginController to test Auth0

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeFit_BE.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet("private")]
+        [Authorize()]
+        public IActionResult Private()
+        {
+            return Ok(new
+            {
+                Message = "Hello from a private endpoint! You need to be authenticated to see this."
+            });
+        }
+        /*
+        public async Task Login(string returnUrl = "/")
+        {
+
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                // Indicate here where Auth0 should redirect the user after a login.
+                // Note that the resulting absolute Uri must be added to the
+                // **Allowed Callback URLs** settings for the app.
+                .WithRedirectUri(returnUrl)
+                .Build();
+
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+            var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+                // Indicate here where Auth0 should redirect the user after a logout.
+                // Note that the resulting absolute Uri must be added in the
+                // **Allowed Logout URLs** settings for the client.
+                .WithRedirectUri(Url.Action("Index", "Home"))
+                .Build();
+
+            await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+        */
+    }
+}
