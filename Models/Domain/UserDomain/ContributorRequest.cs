4d57f168752df3b875cd361c536baab70a526546
Anne E. Larsen <anneelarsen98@gmail.com>
2022-03-24 14:19:38 +0100

    Added Contributer Request table. Added new fields to User table.

---
+++ Models/Domain/UserDomain/ContributorRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeFit_BE.Models.Domain.UserDomain
+{
+    public class ContributorRequest
+    {
+        public int Id { get; set; }
+
+        public int? RequestingUserId { get; set; }
+
+        [ForeignKey("RequestingUserId")]
+        public User RequestingUser { get; set; }
+    }
+}
---
+++ Profiles/ContributorRequestProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MeFit_BE.Models.Domain.UserDomain;
+using MeFit_BE.Models.DTO.ContributorRequest;
+
+namespace MeFit_BE.Profiles
+{
+    public class ContributorRequestProfile : AutoMapper.Profile
+    {
+        public ContributorRequestProfile()
+        {
+            CreateMap<ContributorRequest, ContributorRequestReadDTO>()
+                  .ForMember(crDTO => crDTO.RequestingUser, opt => opt
+                           .MapFrom(cr => cr.RequestingUserId));
+        }
+    }
+}
